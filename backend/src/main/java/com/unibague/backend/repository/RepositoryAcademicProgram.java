f5b63d1e6be023af40ba17ba8b2945f3ff100d57
ST2712 <2220211016@estudiantesunibague.edu.co>
2024-07-19 10:46:22 -0500

    Add: Methods on repository academic program

--- backend/src/main/java/com/unibague/backend/repository/RepositoryAcademicProgram.java
+++ backend/src/main/java/com/unibague/backend/repository/RepositoryAcademicProgram.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface RepositoryAcademicProgram extends JpaRepository<AcademicProgram, Long> {
 
     AcademicProgram findByProgramCode(String programCode);
+    Boolean existsByProgramCode(String programCode);
+    Boolean existsByName(String name);
 }
