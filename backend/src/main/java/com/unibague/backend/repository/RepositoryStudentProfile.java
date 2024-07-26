6e04bf401c9bdc764ce1416e1bb95540e3499118
ST2712 <2220211016@estudiantesunibague.edu.co>
2024-07-26 00:20:34 -0500

    Fix: Some logic related to add a SP
    Some redudant and erroneous logic concerning the methods of adding a student profile was corrected by subdividing methods.

--- backend/src/main/java/com/unibague/backend/repository/RepositoryStudentProfile.java
+++ backend/src/main/java/com/unibague/backend/repository/RepositoryStudentProfile.java
@@ -1,11 +1,13 @@
 package com.unibague.backend.repository;
 
+import com.unibague.backend.model.ResearchSeedbed;
 import com.unibague.backend.model.StudentProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface RepositoryStudentProfile extends JpaRepository<StudentProfile, Long>{
@@ -22,4 +24,10 @@ public interface RepositoryStudentProfile extends JpaRepository<StudentProfile, 
 
     @Query("SELECT sp from StudentProfile sp INNER JOIN sp.userStudent u WHERE u.userIdentification = ?1")
     List<StudentProfile> findByUserIdentification(Long id);
+
+    @Query("SELECT sp from StudentProfile sp WHERE sp.userStudent.userIdentification = ?1 AND sp.assesmentPeriod.id = ?2")
+    Optional<StudentProfile> findByUserIdentificationAndAssesmentPeriodId(String identification, Long assesmentPeriodId);
+
+    @Query("SELECT rs from ResearchSeedbed rs INNER JOIN rs.studentsProfiles sp WHERE sp.userStudent.userIdentification = ?1")
+    Optional<List<ResearchSeedbed>> getStudentProfileResearchSeedbeds(String identification);
 }
--- backend/src/main/java/com/unibague/backend/service/ServiceStudentProfile.java
+++ backend/src/main/java/com/unibague/backend/service/ServiceStudentProfile.java
@@ -1,6 +1,7 @@
 package com.unibague.backend.service;
 
 import com.unibague.backend.model.AcademicProgram;
+import com.unibague.backend.model.AssesmentPeriod;
 import com.unibague.backend.model.ResearchSeedbed;
 import com.unibague.backend.model.StudentProfile;
 import com.unibague.backend.repository.*;
@@ -12,10 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Service
 public class ServiceStudentProfile {
@@ -41,29 +39,40 @@ public class ServiceStudentProfile {
     @Autowired
     ServiceUser serviceUser;
 
+    private Map<String, Object> getJsonByStudentIdentification(String identification) throws Exception{
+        String urlReturn = FetchExternalData.fetchExternalDataFromStudent(identification);
+        return FetchExternalData.fromStringJsonToMap(urlReturn);
+    }
+
+    public Optional<List<ResearchSeedbed>> getStudentProfileResearchSeedbeds(String identification) {
+        return repositoryStudentProfile.getStudentProfileResearchSeedbeds(identification);
+    }
+
+    /**
+     * This method adds a student profile to the database, the method suppose that the student profile is not already in the database
+     * @param studentProfile a json with the student profile information
+     * @return true if the student profile was added successfully, false otherwise
+     */
     public Boolean addStudentProfile(HashMap<String, String> studentProfile) {
         try{
+            Map<String, Object> map = getJsonByStudentIdentification(studentProfile.get("identification_number"));
+            String identification = String.valueOf(map.get(IntegraStudentNomenclature.IDENTIFICATION));
 
-            String urlReturn = FetchExternalData.fetchExternalDataFromStudent(studentProfile.get("identification_number"));
-            Map<String, Object> map = FetchExternalData.fromStringJsonToMap(urlReturn);
-            String identification = String.valueOf(map.get("identification"));
+            Long researchSeedbedId = Long.valueOf(studentProfile.get("research_seedbed_id"));
+            String academicProgramId = (String) map.get("program_code");
+
+            if(repositoryResearchSeedbed.findById(researchSeedbedId).isEmpty() || !repositoryAcademicProgram.existsByProgramCode(academicProgramId)){
+                return false;
+            }
 
             StudentProfile s = studentProfileByIdentification(identification);
+            ResearchSeedbed rs = repositoryResearchSeedbed.findById(researchSeedbedId).get();
+            s.setResearchSeedbeds(List.of(rs));
+            s.setAssesmentPeriod(rs.getAssesmentPeriod());
+            s.setAcademicPrograms(List.of(repositoryAcademicProgram.findByProgramCode(academicProgramId)));
 
-            s.setAssesmentPeriod(repositoryAssesmentPeriod.findById(Long.valueOf(studentProfile.get("assesment_period_id"))).get());
+            repositoryStudentProfile.saveAndFlush(s);
 
-            List<AcademicProgram> list = new ArrayList<>();
-            list.add(repositoryAcademicProgram.findByProgramCode(studentProfile.get("program_codes")));
-            s.setAcademicPrograms(list);
-
-            List<ResearchSeedbed> researchSeedbeds = new ArrayList<>();
-            researchSeedbeds.add(repositoryResearchSeedbed.findById(Long.valueOf(studentProfile.get("research_seedbed_id"))).get());
-            s.setResearchSeedbeds(researchSeedbeds);
-
-            System.out.println(s.toString());
-
-
-            repositoryStudentProfile.save(s);
             return true;
         } catch (Exception e) {
             System.out.printf("Error: %s", e.getMessage());
@@ -108,7 +117,14 @@ public class ServiceStudentProfile {
             if(document == null || document.isEmpty() || document.isBlank()){
                 continue;
             }
-            studentProfile = studentProfileByIdentification(document);
+            try{
+                studentProfile = studentProfileByIdentification(document);
+            }
+            catch (Exception e){
+                System.out.printf("Error: %s", e.getMessage());
+                e.printStackTrace();
+                return false;
+            }
             studentProfile.setAssesmentPeriod(rs.getAssesmentPeriod());
             studentProfile.setResearchSeedbeds(List.of(rs));
 
@@ -123,28 +139,73 @@ public class ServiceStudentProfile {
         return true;
     }
 
-    private StudentProfile studentProfileByIdentification(String identification) {
+    /**
+     * This method return a pre-created student profile with the information of the student with the identification number
+     * @param identification the identification number of the student
+     * @return A pre-created student profile with the information of the student with the identification number
+     */
+    private StudentProfile studentProfileByIdentification(String identification) throws Exception {
 
-        String stringJson = FetchExternalData.fetchExternalDataFromStudent(identification);
-        Map<String, Object> map = FetchExternalData.fromStringJsonToMap(stringJson);
+        Map<String, Object> map = getJsonByStudentIdentification(identification);
 
-        StudentProfile s = new StudentProfile();
+        if(repositoryUser.findByUserIdentification(identification).isEmpty()){
 
-        s.setEmail(String.valueOf(map.get(IntegraStudentNomenclature.EMAIL)));
-        s.setIdentificationNumber(String.valueOf(map.get(IntegraStudentNomenclature.IDENTIFICATION)));
-        s.setName(String.valueOf(map.get(IntegraStudentNomenclature.NAME)));
-        s.setPhoneNumber(String.valueOf(map.get(IntegraStudentNomenclature.TELEPHONE)));
+            HashMap<String, String> userMap = new HashMap<>();
+
+            userMap.put(IntegraStudentNomenclature.IDENTIFICATION, identification);
+            userMap.put(IntegraStudentNomenclature.EMAIL, String.valueOf(map.get(IntegraStudentNomenclature.EMAIL)));
+            userMap.put("isExternalUser", "false");
+
+            serviceUser.addUser(userMap);
+        }
+
+        StudentProfile sp = new StudentProfile();
+
+        sp.setUserStudent(repositoryUser.findByUserIdentification(identification).get());
+        sp.setEmail(String.valueOf(map.get(IntegraStudentNomenclature.EMAIL)));
+        sp.setIdentificationNumber(String.valueOf(map.get(IntegraStudentNomenclature.IDENTIFICATION)));
+        sp.setName(String.valueOf(map.get(IntegraStudentNomenclature.NAME)));
+        sp.setPhoneNumber(String.valueOf(map.get(IntegraStudentNomenclature.TELEPHONE)));
 
         String semester = String.valueOf(map.get(IntegraStudentNomenclature.SEMESTER));
-        s.setSemester(semester.isEmpty() ? 0 : Byte.parseByte(semester));
+        //Some students have the semester as an empty string because they are already graduated
+        sp.setSemester(semester.isEmpty() ? 0 : Byte.parseByte(semester));
 
-        s.setSex(map.get(IntegraStudentNomenclature.SEX).equals("F") ? Sex.FEMALE : Sex.MALE);
-        s.setUserCode(String.valueOf(map.get(IntegraStudentNomenclature.CODE)));
-        s.setWasActive(false);
-        s.setUserStudent(repositoryUser.findByUserIdentification(identification).get());
-        s.setAcademicPrograms(List. of(repositoryAcademicProgram.findByProgramCode(String.valueOf(map.get(IntegraStudentNomenclature.PROGRAM_CODE)))));
+        sp.setSex(map.get(IntegraStudentNomenclature.SEX).equals("F") ? Sex.FEMALE : Sex.MALE);
+        sp.setUserCode(String.valueOf(map.get(IntegraStudentNomenclature.CODE)));
+        sp.setWasActive(false);
 
-        return s;
+        return sp;
+    }
+
+    public Boolean addStudentProfileToAResearchSeedbed(HashMap<String, String> studentProfile){
+        try{
+            Map<String, Object> map = getJsonByStudentIdentification(studentProfile.get(IntegraStudentNomenclature.IDENTIFICATION));
+
+            Long researchSeedbedId = Long.valueOf(studentProfile.get("research_seedbed_id"));
+
+            if(map.get("identification") == null || repositoryResearchSeedbed.findById(researchSeedbedId).isEmpty()){
+                return false;
+            }
+
+            String identification = String.valueOf(map.get("identification"));
+            ResearchSeedbed rs = repositoryResearchSeedbed.findById(researchSeedbedId).get();
+            AssesmentPeriod ap = rs.getAssesmentPeriod();
+
+            StudentProfile sp;
+            if (repositoryStudentProfile.findByUserIdentificationAndAssesmentPeriodId(identification, ap.getId()).isEmpty()) {
+                addStudentProfile(studentProfile);
+            }
+            sp = repositoryStudentProfile.findByUserIdentificationAndAssesmentPeriodId(identification, ap.getId()).get();
+            sp.getResearchSeedbeds().add(rs);
+            repositoryStudentProfile.save(sp);
+            return true;
+        }
+        catch (Exception e){
+            System.out.printf("Error: %s", e.getMessage());
+            e.printStackTrace();
+            return false;
+        }
     }
 
     public List<StudentProfile> getStudentProfilesByResearchSeedbedId(Long researchSeedbedId) {
