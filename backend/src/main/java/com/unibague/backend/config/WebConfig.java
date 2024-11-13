19723c239af5ae98cdd46140df54a8859c19f109
Eder Martínez <ederdmc@hotmail.com>
2024-11-13 06:29:15 +0000

    Implemented Cors for Patch method

--- backend/src/main/java/com/unibague/backend/config/WebConfig.java
+++ backend/src/main/java/com/unibague/backend/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000") // Cambia esto por el origen permitido
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
